23484320536d5e184d17270b1674af0895373dce
GoGangH <rhrkdgus1020@naver.com>
2023-04-11 14:15:28 +0900

    base code to spring

--- Spring/core/src/main/java/hello/core/AppConfig.java
+++ Spring/core/src/main/java/hello/core/AppConfig.java
@@ -9,20 +9,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // Appconfig를 구성정보로 활용
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() { //command option m
+    @Bean
+    public MemberRepository memberRepository() { //command option m
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
